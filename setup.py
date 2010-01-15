8e59d2774e29015e1b0dc33a0a84f2c37ad14d2c
Brian Rosner <brosner@gmail.com>
2010-01-14 19:32:35 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+
+
+setup(
+    name = "agora",
+    version = "0.1.dev1",
+    author = "Eldarion",
+    author_email = "development@eldarion.com",
+    description = "an extensible forum app for Django and Pinax",
+    long_description = open("README.rst").read(),
+    license = "BSD",
+    url = "http://github.com/eldarion/agora",
+    packages = [
+        "agora",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
