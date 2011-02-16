0a715430a1a082676b65a014e70aa3119931e214
Brian Rosner <brosner@gmail.com>
2011-02-16 15:17:47 -0700

    0.1.dev12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "agora",
-    version = "0.1.dev11",
+    version = "0.1.dev12",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "an extensible forum app for Django and Pinax",
