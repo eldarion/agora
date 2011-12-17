efd71ba18a340f4f2c89a1756531d5c04bc9f34f
Brian Rosner <brosner@gmail.com>
2011-12-16 21:28:41 -0700

    added missing forms.py

---
+++ agora/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+
+from agora.models import ForumThread, ForumReply
+
+
+class PostForm(object):
+    
+    subscribe = forms.BooleanField(required=False)
+    
+    def __init__(self, *args, **kwargs):
+        no_subscribe = kwargs.pop("no_subscribe", False)
+        super(PostForm, self).__init__(*args, **kwargs)
+        if no_subscribe:
+            del self.fields["subscribe"]
+
+
+class ThreadForm(PostForm, forms.ModelForm):
+    
+    subscribe = forms.BooleanField(required=False)
+    
+    class Meta:
+        model = ForumThread
+        fields = ["title", "content"]
+
+
+class ReplyForm(PostForm, forms.ModelForm):
+    
+    subscribe = forms.BooleanField(required=False)
+    
+    class Meta:
+        model = ForumReply
+        fields = ["content"]
