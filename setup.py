9800a3bbd114a6f4c66007318efeda40fa643630
Patrick Altman <paltman@gmail.com>
2014-07-18 22:37:38 -0400

    Clean up some style

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from distutils.core import setup
 
 
 setup(
-    name = "agora",
-    version = "0.1.dev16",
-    author = "Eldarion",
-    author_email = "development@eldarion.com",
-    description = "an extensible forum app for Django and Pinax",
-    long_description = open("README.rst").read(),
-    license = "BSD",
-    url = "http://github.com/eldarion/agora",
-    packages = [
+    name="agora",
+    version="0.1.dev16",
+    author="Eldarion",
+    author_email="development@eldarion.com",
+    description="an extensible forum app for Django and Pinax",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    url="http://github.com/eldarion/agora",
+    packages=[
         "agora",
         "agora.templatetags"
     ],
-    install_requires = [
+    install_requires=[
         "django-appconf==0.6"
     ],
-    classifiers = [
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
