7b28356e89106f3d6bc0b6311d2481b96d62499a
James Tauber <jtauber@jtauber.com>
2014-07-19 21:09:52 -0400

    removed code and added link to where it's been moved

---
+++ README.md
@@ -0,0 +1,1 @@
+Agora has been moved to [pinax-forums](https://github.com/pinax/pinax-forums).
--- agora/callbacks.py
+++
@@ -1,6 +0,0 @@
-from django.utils.html import urlize, linebreaks, escape
-from django.utils.safestring import mark_safe
-
-
-def default_text(text):
-    return mark_safe(linebreaks(urlize(escape(text))))
